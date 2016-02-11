ccdf702258c3ac3bef11d5bb08f56c47f1c75427
Sofia <sofiadray@gmail.com>
2016-02-10 21:08:45 -0500

    commented seeds for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,594 +8,594 @@
 
 #make the categories
 
-category = Category.create([
-  { name: 'Advocacy & Human Rights' },
-  { name: 'Animals' },
-  { name: 'Arts & Culture' },
-  { name: 'Board Development' },
-  { name: 'Children & Youth' },
-  { name: 'Community' },
-  { name: 'Computers & Technology' },
-  { name: 'Crisis Support' },
-  { name: 'Disaster Relief' },
-  { name: 'Education & Literacy' },
-  { name: 'Emergency & Safety' },
-  { name: 'Employment' },
-  { name: 'Environment' },
-  { name: 'Faith-Based' },
-  { name: 'Health & Medicine' },
-  { name: 'Homeless & Housing' },
-  { name: 'Hunger' },
-  { name: 'Migrants & Refugees' },
-  { name: 'International' },
-  { name: 'Justice & Legal' },
-  { name: 'LGBT' },
-  { name: 'Media & Broadcasting' },
-  { name: 'People with Disabilities' },
-  { name: 'Politics' },
-  { name: 'Race & Ethnicity' },
-  { name: 'Seniors' },
-  { name: 'Sports & Recreation' },
-  { name: 'Veterans & Military Families' },
-  { name: 'Women'}
-])
+# category = Category.create([
+#   { name: 'Advocacy & Human Rights' },
+#   { name: 'Animals' },
+#   { name: 'Arts & Culture' },
+#   { name: 'Board Development' },
+#   { name: 'Children & Youth' },
+#   { name: 'Community' },
+#   { name: 'Computers & Technology' },
+#   { name: 'Crisis Support' },
+#   { name: 'Disaster Relief' },
+#   { name: 'Education & Literacy' },
+#   { name: 'Emergency & Safety' },
+#   { name: 'Employment' },
+#   { name: 'Environment' },
+#   { name: 'Faith-Based' },
+#   { name: 'Health & Medicine' },
+#   { name: 'Homeless & Housing' },
+#   { name: 'Hunger' },
+#   { name: 'Migrants & Refugees' },
+#   { name: 'International' },
+#   { name: 'Justice & Legal' },
+#   { name: 'LGBT' },
+#   { name: 'Media & Broadcasting' },
+#   { name: 'People with Disabilities' },
+#   { name: 'Politics' },
+#   { name: 'Race & Ethnicity' },
+#   { name: 'Seniors' },
+#   { name: 'Sports & Recreation' },
+#   { name: 'Veterans & Military Families' },
+#   { name: 'Women'}
+# ])
 
 
-Create Cities 
+# Create Cities 
 
-string = "Alabama
+# string = "Alabama
 
-auburn
-birmingham
-dothan
-florence / muscle shoals
-gadsden-anniston
-huntsville / decatur
-mobile
-montgomery
-tuscaloosa
-Alaska
+# auburn
+# birmingham
+# dothan
+# florence / muscle shoals
+# gadsden-anniston
+# huntsville / decatur
+# mobile
+# montgomery
+# tuscaloosa
+# Alaska
 
-anchorage / mat-su
-fairbanks
-kenai peninsula
-southeast alaska
-Arizona
+# anchorage / mat-su
+# fairbanks
+# kenai peninsula
+# southeast alaska
+# Arizona
 
-flagstaff / sedona
-mohave county
-phoenix
-prescott
-show low
-sierra vista
-tucson
-yuma
-Arkansas
+# flagstaff / sedona
+# mohave county
+# phoenix
+# prescott
+# show low
+# sierra vista
+# tucson
+# yuma
+# Arkansas
 
-fayetteville
-fort smith
-jonesboro
-little rock
-texarkana
-California
+# fayetteville
+# fort smith
+# jonesboro
+# little rock
+# texarkana
+# California
 
-bakersfield
-chico
-fresno / madera
-gold country
-hanford-corcoran
-humboldt county
-imperial county
-inland empire
-los angeles
-mendocino county
-merced
-modesto
-monterey bay
-orange county
-palm springs
-redding
-sacramento
-san diego
-san francisco bay area
-san luis obispo
-santa barbara
-santa maria
-siskiyou county
-stockton
-susanville
-ventura county
-visalia-tulare
-yuba-sutter
-Colorado
+# bakersfield
+# chico
+# fresno / madera
+# gold country
+# hanford-corcoran
+# humboldt county
+# imperial county
+# inland empire
+# los angeles
+# mendocino county
+# merced
+# modesto
+# monterey bay
+# orange county
+# palm springs
+# redding
+# sacramento
+# san diego
+# san francisco bay area
+# san luis obispo
+# santa barbara
+# santa maria
+# siskiyou county
+# stockton
+# susanville
+# ventura county
+# visalia-tulare
+# yuba-sutter
+# Colorado
 
-boulder
-colorado springs
-denver
-eastern CO
-fort collins / north CO
-high rockies
-pueblo
-western slope
-Connecticut
+# boulder
+# colorado springs
+# denver
+# eastern CO
+# fort collins / north CO
+# high rockies
+# pueblo
+# western slope
+# Connecticut
 
-eastern CT
-hartford
-new haven
-northwest CT
-Delaware
+# eastern CT
+# hartford
+# new haven
+# northwest CT
+# Delaware
 
-delaware
-District of Columbia
+# delaware
+# District of Columbia
 
-washington
-Florida
+# washington
+# Florida
 
-broward county
-daytona beach
-florida keys
-fort lauderdale
-ft myers / SW florida
-gainesville
-heartland florida
-jacksonville
-lakeland
-miami / dade
-north central FL
-ocala
-okaloosa / walton
-orlando
-panama city
-pensacola
-sarasota-bradenton
-south florida
-space coast
-st augustine
-tallahassee
-tampa bay area
-treasure coast
-palm beach county
-Georgia
+# broward county
+# daytona beach
+# florida keys
+# fort lauderdale
+# ft myers / SW florida
+# gainesville
+# heartland florida
+# jacksonville
+# lakeland
+# miami / dade
+# north central FL
+# ocala
+# okaloosa / walton
+# orlando
+# panama city
+# pensacola
+# sarasota-bradenton
+# south florida
+# space coast
+# st augustine
+# tallahassee
+# tampa bay area
+# treasure coast
+# palm beach county
+# Georgia
 
-albany
-athens
-atlanta
-augusta
-brunswick
-columbus
-macon / warner robins
-northwest GA
-savannah / hinesville
-statesboro
-valdosta
-Hawaii
+# albany
+# athens
+# atlanta
+# augusta
+# brunswick
+# columbus
+# macon / warner robins
+# northwest GA
+# savannah / hinesville
+# statesboro
+# valdosta
+# Hawaii
 
-hawaii
-Idaho
+# hawaii
+# Idaho
 
-boise
-east idaho
-lewiston / clarkston
-twin falls
-Illinois
+# boise
+# east idaho
+# lewiston / clarkston
+# twin falls
+# Illinois
 
-bloomington-normal
-champaign urbana
-chicago
-decatur
-la salle co
-mattoon-charleston
-peoria
-rockford
-southern illinois
-springfield
-western IL
-Indiana
+# bloomington-normal
+# champaign urbana
+# chicago
+# decatur
+# la salle co
+# mattoon-charleston
+# peoria
+# rockford
+# southern illinois
+# springfield
+# western IL
+# Indiana
 
-bloomington
-evansville
-fort wayne
-indianapolis
-kokomo
-lafayette / west lafayette
-muncie / anderson
-richmond
-south bend / michiana
-terre haute
-Iowa
+# bloomington
+# evansville
+# fort wayne
+# indianapolis
+# kokomo
+# lafayette / west lafayette
+# muncie / anderson
+# richmond
+# south bend / michiana
+# terre haute
+# Iowa
 
-ames
-cedar rapids
-des moines
-dubuque
-fort dodge
-iowa city
-mason city
-quad cities
-sioux city
-southeast IA
-waterloo / cedar falls
-Kansas
+# ames
+# cedar rapids
+# des moines
+# dubuque
+# fort dodge
+# iowa city
+# mason city
+# quad cities
+# sioux city
+# southeast IA
+# waterloo / cedar falls
+# Kansas
 
-lawrence
-manhattan
-northwest KS
-salina
-southeast KS
-southwest KS
-topeka
-wichita
-Kentucky
+# lawrence
+# manhattan
+# northwest KS
+# salina
+# southeast KS
+# southwest KS
+# topeka
+# wichita
+# Kentucky
 
-bowling green
-eastern kentucky
-lexington
-louisville
-owensboro
-western KY
-Louisiana
+# bowling green
+# eastern kentucky
+# lexington
+# louisville
+# owensboro
+# western KY
+# Louisiana
 
-baton rouge
-central louisiana
-houma
-lafayette
-lake charles
-monroe
-new orleans
-shreveport
-Maine
+# baton rouge
+# central louisiana
+# houma
+# lafayette
+# lake charles
+# monroe
+# new orleans
+# shreveport
+# Maine
 
-maine
-Maryland
+# maine
+# Maryland
 
-annapolis
-baltimore
-eastern shore
-frederick
-southern maryland
-western maryland
-Massachusetts
+# annapolis
+# baltimore
+# eastern shore
+# frederick
+# southern maryland
+# western maryland
+# Massachusetts
 
-boston
-cape cod / islands
-south coast
-western massachusetts
-worcester / central MA
-Michigan
+# boston
+# cape cod / islands
+# south coast
+# western massachusetts
+# worcester / central MA
+# Michigan
 
-ann arbor
-battle creek
-central michigan
-detroit metro
-flint
-grand rapids
-holland
-jackson
-kalamazoo
-lansing
-monroe
-muskegon
-northern michigan
-port huron
-saginaw-midland-baycity
-southwest michigan
-the thumb
-upper peninsula
-Minnesota
+# ann arbor
+# battle creek
+# central michigan
+# detroit metro
+# flint
+# grand rapids
+# holland
+# jackson
+# kalamazoo
+# lansing
+# monroe
+# muskegon
+# northern michigan
+# port huron
+# saginaw-midland-baycity
+# southwest michigan
+# the thumb
+# upper peninsula
+# Minnesota
 
-bemidji
-brainerd
-duluth / superior
-mankato
-minneapolis / st paul
-rochester
-southwest MN
-st cloud
-Mississippi
+# bemidji
+# brainerd
+# duluth / superior
+# mankato
+# minneapolis / st paul
+# rochester
+# southwest MN
+# st cloud
+# Mississippi
 
-gulfport / biloxi
-hattiesburg
-jackson
-meridian
-north mississippi
-southwest MS
-Missouri
+# gulfport / biloxi
+# hattiesburg
+# jackson
+# meridian
+# north mississippi
+# southwest MS
+# Missouri
 
-columbia / jeff city
-joplin
-kansas city
-kirksville
-lake of the ozarks
-southeast missouri
-springfield
-st joseph
-st louis
-Montana
+# columbia / jeff city
+# joplin
+# kansas city
+# kirksville
+# lake of the ozarks
+# southeast missouri
+# springfield
+# st joseph
+# st louis
+# Montana
 
-billings
-bozeman
-butte
-great falls
-helena
-kalispell
-missoula
-eastern montana
-Nebraska
+# billings
+# bozeman
+# butte
+# great falls
+# helena
+# kalispell
+# missoula
+# eastern montana
+# Nebraska
 
-grand island
-lincoln
-north platte
-omaha / council bluffs
-scottsbluff / panhandle
-Nevada
+# grand island
+# lincoln
+# north platte
+# omaha / council bluffs
+# scottsbluff / panhandle
+# Nevada
 
-elko
-las vegas
-reno / tahoe
-New Hampshire
+# elko
+# las vegas
+# reno / tahoe
+# New Hampshire
 
-new hampshire
-New Jersey
+# new hampshire
+# New Jersey
 
-central NJ
-jersey shore
-north jersey
-south jersey
-New Mexico
+# central NJ
+# jersey shore
+# north jersey
+# south jersey
+# New Mexico
 
-albuquerque
-clovis / portales
-farmington
-las cruces
-roswell / carlsbad
-santa fe / taos
-New York
+# albuquerque
+# clovis / portales
+# farmington
+# las cruces
+# roswell / carlsbad
+# santa fe / taos
+# New York
 
-albany
-binghamton
-buffalo
-catskills
-chautauqua
-elmira-corning
-finger lakes
-glens falls
-hudson valley
-ithaca
-long island
-new york city
-oneonta
-plattsburgh-adirondacks
-potsdam-canton-massena
-rochester
-syracuse
-twin tiers NY/PA
-utica-rome-oneida
-watertown
-North Carolina
+# albany
+# binghamton
+# buffalo
+# catskills
+# chautauqua
+# elmira-corning
+# finger lakes
+# glens falls
+# hudson valley
+# ithaca
+# long island
+# new york city
+# oneonta
+# plattsburgh-adirondacks
+# potsdam-canton-massena
+# rochester
+# syracuse
+# twin tiers NY/PA
+# utica-rome-oneida
+# watertown
+# North Carolina
 
-asheville
-boone
-charlotte
-eastern NC
-fayetteville
-greensboro
-hickory / lenoir
-jacksonville
-outer banks
-raleigh / durham / CH
-wilmington
-winston-salem
-North Dakota
+# asheville
+# boone
+# charlotte
+# eastern NC
+# fayetteville
+# greensboro
+# hickory / lenoir
+# jacksonville
+# outer banks
+# raleigh / durham / CH
+# wilmington
+# winston-salem
+# North Dakota
 
-bismarck
-fargo / moorhead
-grand forks
-north dakota
-Ohio
+# bismarck
+# fargo / moorhead
+# grand forks
+# north dakota
+# Ohio
 
-akron / canton
-ashtabula
-athens
-chillicothe
-cincinnati
-cleveland
-columbus
-dayton / springfield
-lima / findlay
-mansfield
-sandusky
-toledo
-tuscarawas co
-youngstown
-zanesville / cambridge
-Oklahoma
+# akron / canton
+# ashtabula
+# athens
+# chillicothe
+# cincinnati
+# cleveland
+# columbus
+# dayton / springfield
+# lima / findlay
+# mansfield
+# sandusky
+# toledo
+# tuscarawas co
+# youngstown
+# zanesville / cambridge
+# Oklahoma
 
-lawton
-northwest OK
-oklahoma city
-stillwater
-tulsa
-Oregon
+# lawton
+# northwest OK
+# oklahoma city
+# stillwater
+# tulsa
+# Oregon
 
-bend
-corvallis/albany
-east oregon
-eugene
-klamath falls
-medford-ashland
-oregon coast
-portland
-roseburg
-salem
-Pennsylvania
+# bend
+# corvallis/albany
+# east oregon
+# eugene
+# klamath falls
+# medford-ashland
+# oregon coast
+# portland
+# roseburg
+# salem
+# Pennsylvania
 
-altoona-johnstown
-cumberland valley
-erie
-harrisburg
-lancaster
-lehigh valley
-meadville
-philadelphia
-pittsburgh
-poconos
-reading
-scranton / wilkes-barre
-state college
-williamsport
-york
-Rhode Island
+# altoona-johnstown
+# cumberland valley
+# erie
+# harrisburg
+# lancaster
+# lehigh valley
+# meadville
+# philadelphia
+# pittsburgh
+# poconos
+# reading
+# scranton / wilkes-barre
+# state college
+# williamsport
+# york
+# Rhode Island
 
-rhode island
-South Carolina
+# rhode island
+# South Carolina
 
-charleston
-columbia
-florence
-greenville / upstate
-hilton head
-myrtle beach
-South Dakota
+# charleston
+# columbia
+# florence
+# greenville / upstate
+# hilton head
+# myrtle beach
+# South Dakota
 
-northeast SD
-pierre / central SD
-rapid city / west SD
-sioux falls / SE SD
-south dakota
-Tennessee
+# northeast SD
+# pierre / central SD
+# rapid city / west SD
+# sioux falls / SE SD
+# south dakota
+# Tennessee
 
-chattanooga
-clarksville
-cookeville
-jackson
-knoxville
-memphis
-nashville
-tri-cities
-Texas
+# chattanooga
+# clarksville
+# cookeville
+# jackson
+# knoxville
+# memphis
+# nashville
+# tri-cities
+# Texas
 
-abilene
-amarillo
-austin
-beaumont / port arthur
-brownsville
-college station
-corpus christi
-dallas / fort worth
-deep east texas
-del rio / eagle pass
-el paso
-galveston
-houston
-killeen / temple / ft hood
-laredo
-lubbock
-mcallen / edinburg
-odessa / midland
-san angelo
-san antonio
-san marcos
-southwest TX
-texoma
-tyler / east TX
-victoria
-waco
-wichita falls
-Utah
+# abilene
+# amarillo
+# austin
+# beaumont / port arthur
+# brownsville
+# college station
+# corpus christi
+# dallas / fort worth
+# deep east texas
+# del rio / eagle pass
+# el paso
+# galveston
+# houston
+# killeen / temple / ft hood
+# laredo
+# lubbock
+# mcallen / edinburg
+# odessa / midland
+# san angelo
+# san antonio
+# san marcos
+# southwest TX
+# texoma
+# tyler / east TX
+# victoria
+# waco
+# wichita falls
+# Utah
 
-logan
-ogden-clearfield
-provo / orem
-salt lake city
-st george
-Vermont
+# logan
+# ogden-clearfield
+# provo / orem
+# salt lake city
+# st george
+# Vermont
 
-vermont
-Virginia
+# vermont
+# Virginia
 
-charlottesville
-danville
-fredericksburg
-hampton roads
-harrisonburg
-lynchburg
-new river valley
-richmond
-roanoke
-southwest VA
-winchester
-Washington
+# charlottesville
+# danville
+# fredericksburg
+# hampton roads
+# harrisonburg
+# lynchburg
+# new river valley
+# richmond
+# roanoke
+# southwest VA
+# winchester
+# Washington
 
-bellingham
-kennewick-pasco-richland
-moses lake
-olympic peninsula
-pullman / moscow
-seattle-tacoma
-skagit / island / SJI
-spokane / coeur d'alene
-wenatchee
-yakima
-West Virginia
+# bellingham
+# kennewick-pasco-richland
+# moses lake
+# olympic peninsula
+# pullman / moscow
+# seattle-tacoma
+# skagit / island / SJI
+# spokane / coeur d'alene
+# wenatchee
+# yakima
+# West Virginia
 
-charleston
-eastern panhandle
-huntington-ashland
-morgantown
-northern panhandle
-parkersburg-marietta
-southern WV
-west virginia (old)
-Wisconsin
+# charleston
+# eastern panhandle
+# huntington-ashland
+# morgantown
+# northern panhandle
+# parkersburg-marietta
+# southern WV
+# west virginia (old)
+# Wisconsin
 
-appleton-oshkosh-FDL
-eau claire
-green bay
-janesville
-kenosha-racine
-la crosse
-madison
-milwaukee
-northern WI
-sheboygan
-wausau
-Wyoming
+# appleton-oshkosh-FDL
+# eau claire
+# green bay
+# janesville
+# kenosha-racine
+# la crosse
+# madison
+# milwaukee
+# northern WI
+# sheboygan
+# wausau
+# Wyoming
 
-wyoming
-Territories
+# wyoming
+# Territories
 
-guam-micronesia
-puerto rico
-U.S. virgin islands"
+# guam-micronesia
+# puerto rico
+# U.S. virgin islands"
 
-arrays = []
-idxs = []
-array = string.split("\n").delete_if{|a| a == ""}
-array.each do |a|
-    if a[0] == a[0].upcase
-     idxs << array.index(a)
-    end
-end
+# arrays = []
+# idxs = []
+# array = string.split("\n").delete_if{|a| a == ""}
+# array.each do |a|
+#     if a[0] == a[0].upcase
+#      idxs << array.index(a)
+#     end
+# end
 
-i = 0
-while i < 52
-	arrays << array.slice(idxs[i]..idxs[i+1]-1)
-	i += 1
-end
+# i = 0
+# while i < 52
+# 	arrays << array.slice(idxs[i]..idxs[i+1]-1)
+# 	i += 1
+# end
 
-arrays.each do |a|
-	state = a.shift
-	for i in 1..a.length
-		unless a[i] == nil
-		city = City.create(name:a[i], state: state, country: "United States")
-		if city.save
-			puts "name: #{a[i]}, state: #{state}, country:#{"United States"} Created"
-		else puts "city not saved"
-		end
-		end
-	end
-puts "-------------------------------------"
-end
+# arrays.each do |a|
+# 	state = a.shift
+# 	for i in 1..a.length
+# 		unless a[i] == nil
+# 		city = City.create(name:a[i], state: state, country: "United States")
+# 		if city.save
+# 			puts "name: #{a[i]}, state: #{state}, country:#{"United States"} Created"
+# 		else puts "city not saved"
+# 		end
+# 		end
+# 	end
+# puts "-------------------------------------"
+# end
 
 
 ###################################
